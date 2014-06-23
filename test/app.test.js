f6277749698bbefcfa7e14f0cf0f68bf5046c129
David Wood <david.p.wood@gmail.com>
2014-06-23 13:58:54 +0100

    Adds user accounts to the mock server and requires request auth.

--- test/app.test.js
+++ test/app.test.js
@@ -2,8 +2,16 @@ var request = require('supertest'),
     random = require('./random'),
     config = require('../config').development,
     mockserver = require('../'),
-    assert = require('assert');
+    assert = require('assert'),
+    uuid = require('node-uuid');
 
+
+function createRandomUser() {
+  return {
+    username: random.string(20),
+    apitoken: uuid.v4()
+  }
+}
 
 function createRandomZone() {
   return {
@@ -35,7 +43,7 @@ function expectedZoneResponse(zoneid, zone) {
   };
 }
 
-function expectedZoneRecordResponse(zoneid, zone, recordid, record) {
+function expectedRecordResponse(zoneid, zone, recordid, record) {
   return {
     "id": recordid,
     "name": record["name"] + '.' + zone["name"] + '.',
@@ -48,20 +56,75 @@ function expectedZoneRecordResponse(zoneid, zone, recordid, record) {
   };
 }
 
+before(function (done) {
+
+  server = mockserver.app.listen(config.server.port, config.server.host, function () {
+    serverurl = "http://" + config.server.host + ":" + config.server.port;
+    done();
+  });
+
+});
+
+after(function (done) {
+  server.close(done);
+});
+
 
 describe('Mock PointHQ Server', function () {
 
-  before(function (done) {
+  describe('GET /zones without authorization header', function () {
 
-    server = mockserver.app.listen(config.server.port, config.server.host, function () {
-      serverurl = "http://" + config.server.host + ":" + config.server.port;
-      done();
+    var zone;
+
+    before(function () {
+      mockserver.reset();
+
+      zone = createRandomZone();
+
+      mockserver.addZone(zone);
+    });
+
+    it('should return 302 Found with Location set', function (done) {
+
+      request(serverurl)
+        .get('/zones')
+        .expect(302)
+        .expect('Location', 'https://sso.copper.io/api/v1/sessions/validate?service=https%3A%2F%2Fpointhq.com%2Fzones')
+        .expect({})
+        .end(done);
+
     });
 
   });
 
-  after(function (done) {
-    server.close(done);
+  describe('GET /zones with incorrect authorization header', function () {
+
+    var zone;
+
+    before(function () {
+      mockserver.reset();
+
+      zone = createRandomZone();
+      user = createRandomUser();
+
+      mockserver.addZone(zone);
+
+      mockserver.addUser(createRandomUser());
+      mockserver.addUser(user);
+      mockserver.addUser(createRandomUser());
+    });
+
+    it('should return 403 Found with Location set', function (done) {
+
+      request(serverurl)
+        .get('/zones')
+        .auth(user.username, uuid.v4())
+        .expect(403)
+        .expect({})
+        .end(done);
+
+    });
+
   });
 
   describe('GET /zones', function () {
@@ -73,15 +136,18 @@ describe('Mock PointHQ Server', function () {
 
       zone1 = createRandomZone();
       zone2 = createRandomZone();
+      user = createRandomUser();
 
       mockserver.addZone(zone1);
       mockserver.addZone(zone2);
+      mockserver.addUser(user);
     });
 
     it('should return both zones', function (done) {
 
       request(serverurl)
         .get('/zones')
+        .auth(user.username, user.apitoken)
         .expect(200)
         .expect([{ zone: expectedZoneResponse(1, zone1) },{ zone: expectedZoneResponse(2, zone2) }])
         .end(done);
@@ -99,15 +165,18 @@ describe('Mock PointHQ Server', function () {
 
       zone1 = createRandomZone();
       zone2 = createRandomZone();
+      user = createRandomUser();
 
       mockserver.addZone(zone1);
       mockserver.addZone(zone2);
+      mockserver.addUser(user);
     });
 
     it('should return the first zone', function (done) {
 
       request(serverurl)
         .get('/zones/1')
+        .auth(user.username, user.apitoken)
         .expect(200)
         .expect({ zone: expectedZoneResponse(1, zone1) })
         .end(done);
@@ -125,15 +194,18 @@ describe('Mock PointHQ Server', function () {
 
       zone1 = createRandomZone();
       zone2 = createRandomZone();
+      user = createRandomUser();
 
       mockserver.addZone(zone1);
       mockserver.addZone(zone2);
+      mockserver.addUser(user);
     });
 
     it('should return the second zone', function (done) {
 
       request(serverurl)
         .get('/zones/2')
+        .auth(user.username, user.apitoken)
         .expect(200)
         .expect({ zone: expectedZoneResponse(2, zone2) })
         .end(done);
@@ -153,19 +225,22 @@ describe('Mock PointHQ Server', function () {
       zone = createRandomZone();
       record1 = createRandomZoneRecord(zone);
       record2 = createRandomZoneRecord(zone);
+      user = createRandomUser();
 
       mockserver.addZone(zone, [record1, record2]);
+      mockserver.addUser(user);
     });
 
     it('should return the first zone\'s records', function (done) {
 
       request(serverurl)
         .get('/zones/1/records')
+        .auth(user.username, user.apitoken)
         .expect(200)
         .expect([{
-          zone_record: expectedZoneRecordResponse(1, zone, 1, record1)
+          zone_record: expectedRecordResponse(1, zone, 1, record1)
         },{
-          zone_record: expectedZoneRecordResponse(1, zone, 2, record2)
+          zone_record: expectedRecordResponse(1, zone, 2, record2)
         }])
         .end(done);
 
@@ -184,20 +259,23 @@ describe('Mock PointHQ Server', function () {
       zone = createRandomZone();
       record1 = createRandomZoneRecord(zone);
       record2 = createRandomZoneRecord(zone);
+      user = createRandomUser();
 
       mockserver.addZone(createRandomZone());
       mockserver.addZone(zone, [record1, record2]);
+      mockserver.addUser(user);
     });
 
     it('should return the second zone\'s records', function (done) {
 
       request(serverurl)
         .get('/zones/2/records')
+        .auth(user.username, user.apitoken)
         .expect(200)
         .expect([{
-          zone_record: expectedZoneRecordResponse(2, zone, 1, record1)
+          zone_record: expectedRecordResponse(2, zone, 1, record1)
         },{
-          zone_record: expectedZoneRecordResponse(2, zone, 2, record2)
+          zone_record: expectedRecordResponse(2, zone, 2, record2)
         }])
         .end(done);
 
@@ -207,10 +285,21 @@ describe('Mock PointHQ Server', function () {
 
   describe('GET /zone/unknown_zone', function () {
 
+    var user;
+
+    before(function () {
+      mockserver.reset();
+
+      user = createRandomUser();
+
+      mockserver.addUser(user);
+    });
+
     it('should return 404', function (done) {
 
       request(serverurl)
         .get('/zone/unknown_zone')
+        .auth(user.username, user.apitoken)
         .expect(404)
         .end(done);
 
@@ -220,10 +309,21 @@ describe('Mock PointHQ Server', function () {
 
   describe('GET /zone/1 when no zones exist', function () {
 
+    var user;
+
+    before(function () {
+      mockserver.reset();
+
+      user = createRandomUser();
+
+      mockserver.addUser(user);
+    });
+
     it('should return 404', function (done) {
 
       request(serverurl)
         .get('/zone/1')
+        .auth(user.username, user.apitoken)
         .expect(404)
         .end(done);
 
@@ -233,10 +333,21 @@ describe('Mock PointHQ Server', function () {
 
   describe('GET /unknown_path', function () {
 
+    var user;
+
+    before(function () {
+      mockserver.reset();
+
+      user = createRandomUser();
+
+      mockserver.addUser(user);
+    });
+
     it('should return 404', function (done) {
 
       request(serverurl)
         .get('/unknown_path')
+        .auth(user.username, user.apitoken)
         .expect(404)
         .end(done);
 
@@ -252,15 +363,18 @@ describe('Mock PointHQ Server', function () {
       mockserver.reset();
 
       zone = createRandomZone();
+      user = createRandomUser();
 
       mockserver.addZone(createRandomZone());
       mockserver.addZone(zone);
+      mockserver.addUser(user);
     });
 
     it('should delete the second zone', function (done) {
 
       request(serverurl)
         .delete('/zones/2')
+        .auth(user.username, user.apitoken)
         .expect(200)
         .expect({ zone: { status: "OK" }})
         .end(done);
@@ -286,7 +400,10 @@ describe('Mock PointHQ Server', function () {
       group = "new group";
 
       zone = createRandomZone();
+      user = createRandomUser();
+
       mockserver.addZone(zone);
+      mockserver.addUser(user);
 
       zone.group = group;
     });
@@ -295,6 +412,7 @@ describe('Mock PointHQ Server', function () {
 
       request(serverurl)
         .put('/zones/1')
+        .auth(user.username, user.apitoken)
         .send({ zone: { group: group }})
         .expect(202)
         .expect({ zone: expectedZoneResponse(1, zone) })
@@ -322,8 +440,11 @@ describe('Mock PointHQ Server', function () {
       ipAddress = random.ipAddress();
 
       zone = createRandomZone();
+      user = createRandomUser();
       record = createRandomZoneRecord(zone);
+      
       mockserver.addZone(zone, [record]);
+      mockserver.addUser(user);
 
       record.data = ipAddress;
     });
@@ -332,9 +453,10 @@ describe('Mock PointHQ Server', function () {
 
       request(serverurl)
         .put('/zones/1/records/1')
+        .auth(user.username, user.apitoken)
         .send({ zone_record: { data: ipAddress }})
         .expect(202)
-        .expect({ zone_record: expectedZoneRecordResponse(1, zone, 1, record) })
+        .expect({ zone_record: expectedRecordResponse(1, zone, 1, record) })
         .end(done);
 
     });
