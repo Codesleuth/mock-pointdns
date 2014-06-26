dfd02036acb8a889c24355acc1a0df0a0cdef7a0
David Wood <david.p.wood@gmail.com>
2014-06-27 00:20:40 +0100

    Moved zone storage and manipulation into a repository.

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,6 @@
-var request = require('supertest'),
-    random = require('./random'),
+var request = require('supertest')
+    util = require('util'),
+    random = require('../lib/random'),
     config = require('../config').development,
     mockserver = require('../'),
     assert = require('assert'),
@@ -15,6 +16,7 @@ function createRandomUser() {
 
 function createRandomZone() {
   return {
+    "id": random.number(1, 999),
     "name": random.string(10) + '.' + random.choice('com', 'co', 'co.uk'),
     "group": random.string(),
     "user-id": random.number(),
@@ -24,6 +26,7 @@ function createRandomZone() {
 
 function createRandomZoneRecord() {
   return {
+    "id": random.number(1, 999),
     "name": random.string(10),
     "data": random.ipAddress(),
     "aux": random.string(),
@@ -33,33 +36,36 @@ function createRandomZoneRecord() {
   }
 }
 
-function expectedZoneResponse(zoneid, zone) {
+function expectedZoneResponse(zone) {
   return {
-    "id": zoneid,
-    "name": zone["name"],
-    "group": zone["group"],
+    "id": zone.id,
+    "name": zone.name,
+    "group": zone.group,
     "user-id": zone["user-id"],
-    "ttl": zone["ttl"]
+    "ttl": zone.ttl
   };
 }
 
-function expectedRecordResponse(zoneid, zone, recordid, record) {
+function expectedRecordResponse(zone, record) {
   return {
-    "id": recordid,
-    "name": record["name"] + '.' + zone["name"] + '.',
-    "data": record["data"],
-    "aux": record["aux"],
-    "record_type": record["record_type"],
-    "redirect_to": record["redirect_to"],
-    "ttl": record["ttl"],
-    "zone_id": zoneid
+    "id": record.id,
+    "name": record.name + '.' + zone.name + '.',
+    "data": record.data,
+    "aux": record.aux,
+    "record_type": record.record_type,
+    "redirect_to": record.redirect_to,
+    "ttl": record.ttl,
+    "zone_id": zone.id
   };
 }
+
+var server;
+var serverUrl;
 
 before(function (done) {
 
   server = mockserver.app.listen(config.server.port, config.server.host, function () {
-    serverurl = "http://" + config.server.host + ":" + config.server.port;
+    serverUrl = "http://" + config.server.host + ":" + config.server.port;
     done();
   });
 
@@ -86,7 +92,7 @@ describe('Unknown Routes', function () {
 
     it('should return 404', function (done) {
 
-      request(serverurl)
+      request(serverUrl)
         .get('/unknown_path')
         .auth(user.username, user.password)
         .expect(404)
@@ -110,7 +116,7 @@ describe('Authorization', function () {
 
     it('should return 302 Found with Location header', function (done) {
 
-      request(serverurl)
+      request(serverUrl)
         .get('/zones')
         .expect(302)
         .expect('Location', 'https://sso.copper.io/api/v1/sessions/validate?service=https%3A%2F%2Fpointhq.com%2Fzones')
@@ -121,17 +127,22 @@ describe('Authorization', function () {
 
   });
 
-  describe('GET /zones/1 with missing Authorization header', function () {
+  describe('GET /zones/:id with missing Authorization header', function () {
+
+    var zone;
 
     before(function () {
       mockserver.reset();
+
+      zone = createRandomZone();
+
       mockserver.addZone(createRandomZone());
     });
 
     it('should return 404 Not Found', function (done) {
 
-      request(serverurl)
-        .get('/zones/1')
+      request(serverUrl)
+        .get('/zones/' + zone.id)
         .expect(404)
         .end(done);
 
@@ -159,7 +170,7 @@ describe('Authorization', function () {
 
       it('should return 200 OK', function (done) {
 
-        request(serverurl)
+        request(serverUrl)
           .get('/zones')
           .auth(user.username, user.password)
           .expect(200)
@@ -186,7 +197,7 @@ describe('Authorization', function () {
 
       it('should return 403 Forbidden with \'Access Denied\' body', function (done) {
 
-        request(serverurl)
+        request(serverUrl)
           .get('/zones')
           .auth(random.string(10), user.password)
           .expect(403)
@@ -212,7 +223,7 @@ describe('Authorization', function () {
 
       it('should return 403 Forbidden', function (done) {
 
-        request(serverurl)
+        request(serverUrl)
           .get('/zones')
           .auth(user.username, random.string(50))
           .expect(403)
@@ -244,7 +255,7 @@ describe('Authorization', function () {
 
       it('should return 200 OK', function (done) {
 
-        request(serverurl)
+        request(serverUrl)
           .get('/zones')
           .auth('', apitoken)
           .expect(200)
@@ -271,7 +282,7 @@ describe('Authorization', function () {
 
       it('should return 200 OK', function (done) {
 
-        request(serverurl)
+        request(serverUrl)
           .get('/zones')
           .auth(user.username, apitoken)
           .expect(200)
@@ -295,7 +306,7 @@ describe('Authorization', function () {
 
       it('should return 200 OK', function (done) {
 
-        request(serverurl)
+        request(serverUrl)
           .get('/zones')
           .auth(random.string(), apitoken)
           .expect(200)
@@ -331,18 +342,18 @@ describe('Zones', function () {
 
     it('should return both zones', function (done) {
 
-      request(serverurl)
+      request(serverUrl)
         .get('/zones')
         .auth(user.username, user.password)
         .expect(200)
-        .expect([{ zone: expectedZoneResponse(1, zone1) },{ zone: expectedZoneResponse(2, zone2) }])
+        .expect([{ zone: expectedZoneResponse(zone1) },{ zone: expectedZoneResponse(zone2) }])
         .end(done);
 
     });
 
   });
 
-  describe('GET /zone/1', function () {
+  describe('GET /zone/:id', function () {
 
     var zone1, zone2;
     var user;
@@ -359,50 +370,31 @@ describe('Zones', function () {
       mockserver.addUser(user);
     });
 
-    it('should return the first zone', function (done) {
+    it('should return the second zone as expected', function (done) {
 
-      request(serverurl)
-        .get('/zones/1')
+      request(serverUrl)
+        .get('/zones/' + zone2.id)
         .auth(user.username, user.password)
         .expect(200)
-        .expect({ zone: expectedZoneResponse(1, zone1) })
+        .expect({ zone: expectedZoneResponse(zone2) })
+        .end(done);
+
+    });
+
+    it('should return the first zone as expected', function (done) {
+
+      request(serverUrl)
+        .get('/zones/' + zone1.id)
+        .auth(user.username, user.password)
+        .expect(200)
+        .expect({ zone: expectedZoneResponse(zone1) })
         .end(done);
 
     });
 
   });
 
-  describe('GET /zone/2', function () {
-
-    var zone1, zone2;
-    var user;
-
-    before(function () {
-      mockserver.reset();
-
-      zone1 = createRandomZone();
-      zone2 = createRandomZone();
-      user = createRandomUser();
-
-      mockserver.addZone(zone1);
-      mockserver.addZone(zone2);
-      mockserver.addUser(user);
-    });
-
-    it('should return the second zone', function (done) {
-
-      request(serverurl)
-        .get('/zones/2')
-        .auth(user.username, user.password)
-        .expect(200)
-        .expect({ zone: expectedZoneResponse(2, zone2) })
-        .end(done);
-
-    });
-
-  });
-
-  describe('DELETE /zone/2', function () {
+  describe('DELETE /zone/:id', function () {
 
     var zone;
     var user;
@@ -420,8 +412,8 @@ describe('Zones', function () {
 
     it('should delete the second zone', function (done) {
 
-      request(serverurl)
-        .delete('/zones/2')
+      request(serverUrl)
+        .delete(util.format('/zones/%d', zone.id))
         .auth(user.username, user.password)
         .expect(200)
         .expect({ zone: { status: "OK" }})
@@ -437,7 +429,7 @@ describe('Zones', function () {
 
   });
 
-  describe('PUT /zone/1', function () {
+  describe('PUT /zone/:id', function () {
 
     var zone;
     var group;
@@ -459,19 +451,20 @@ describe('Zones', function () {
 
     it('should change the group of the first zone', function (done) {
 
-      request(serverurl)
-        .put('/zones/1')
+      request(serverUrl)
+        .put(util.format('/zones/%d', zone.id))
         .auth(user.username, user.password)
         .send({ zone: { group: group }})
         .expect(202)
-        .expect({ zone: expectedZoneResponse(1, zone) })
+        .expect({ zone: expectedZoneResponse(zone) })
         .end(done);
 
     });
 
     it('should have changed the group for the first zone', function () {
 
-      assert.equal(mockserver.getZone(1).group, group);
+      var actualZone = mockserver.getZone(zone.id);
+      assert.equal(actualZone.group, group);
 
     });
 
@@ -491,7 +484,7 @@ describe('Zones', function () {
 
     it('should return 404', function (done) {
 
-      request(serverurl)
+      request(serverUrl)
         .get('/zone/unknown_zone')
         .auth(user.username, user.password)
         .expect(404)
@@ -515,7 +508,7 @@ describe('Zones', function () {
 
     it('should return 404', function (done) {
 
-      request(serverurl)
+      request(serverUrl)
         .get('/zone/1')
         .auth(user.username, user.password)
         .expect(404)
@@ -529,42 +522,7 @@ describe('Zones', function () {
 
 describe('Zone Records', function () {
 
-  describe('GET /zone/1/records', function () {
-
-    var zone;
-    var record1, record2;
-    var user;
-
-    before(function () {
-      mockserver.reset();
-
-      zone = createRandomZone();
-      record1 = createRandomZoneRecord(zone);
-      record2 = createRandomZoneRecord(zone);
-      user = createRandomUser();
-
-      mockserver.addZone(zone, [record1, record2]);
-      mockserver.addUser(user);
-    });
-
-    it('should return the first zone\'s records', function (done) {
-
-      request(serverurl)
-        .get('/zones/1/records')
-        .auth(user.username, user.password)
-        .expect(200)
-        .expect([{
-          zone_record: expectedRecordResponse(1, zone, 1, record1)
-        },{
-          zone_record: expectedRecordResponse(1, zone, 2, record2)
-        }])
-        .end(done);
-
-    });
-
-  });
-
-  describe('GET /zone/2/records', function () {
+  describe('GET /zone/:id/records', function () {
 
     var zone;
     var record1, record2;
@@ -580,19 +538,20 @@ describe('Zone Records', function () {
 
       mockserver.addZone(createRandomZone());
       mockserver.addZone(zone, [record1, record2]);
+      mockserver.addZone(createRandomZone());
       mockserver.addUser(user);
     });
 
-    it('should return the second zone\'s records', function (done) {
+    it('should return the zone\'s records', function (done) {
 
-      request(serverurl)
-        .get('/zones/2/records')
+      request(serverUrl)
+        .get(util.format('/zones/%d/records', zone.id))
         .auth(user.username, user.password)
         .expect(200)
         .expect([{
-          zone_record: expectedRecordResponse(2, zone, 1, record1)
+          zone_record: expectedRecordResponse(zone, record1)
         },{
-          zone_record: expectedRecordResponse(2, zone, 2, record2)
+          zone_record: expectedRecordResponse(zone, record2)
         }])
         .end(done);
 
@@ -600,7 +559,7 @@ describe('Zone Records', function () {
 
   });
 
-  describe('PUT /zones/1/records/1', function () {
+  describe('PUT /zones/:id/records/:id', function () {
 
     var zone;
     var record;
@@ -624,19 +583,20 @@ describe('Zone Records', function () {
 
     it('should change the data of the first zone\'s first record', function (done) {
 
-      request(serverurl)
-        .put('/zones/1/records/1')
+      request(serverUrl)
+        .put(util.format('/zones/%d/records/%d', zone.id, record.id))
         .auth(user.username, user.password)
         .send({ zone_record: { data: ipAddress }})
         .expect(202)
-        .expect({ zone_record: expectedRecordResponse(1, zone, 1, record) })
+        .expect({ zone_record: expectedRecordResponse(zone, record) })
         .end(done);
 
     });
 
     it('should have changed the data of the first zone\'s first record', function () {
-
-      assert.equal(mockserver.getRecord(1, 1).data, ipAddress);
+      
+      var actualRecord = mockserver.getRecord(zone.id, record.id);
+      assert.equal(actualRecord.data, ipAddress);
 
     });
 
---
+++ test/zoneRepository.test.js
@@ -0,0 +1,415 @@
+var ZoneRepository = require('../lib/zoneRepository'),
+    random = require('../lib/random'),
+    assert = require('assert');
+
+
+function createRandomRecordType() {
+  return random.choice('A', 'CNAME', 'MX', 'TXT', 'NS', 'SRV', 'AAAA', 'SSHFP', 'PTR', 'ALIAS');
+}
+
+describe('Zone Repository', function () {
+
+  describe('#addZone with zone', function () {
+
+    var repo;
+    var expectedZone;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      expectedZone = {
+        id: random.number(1, 9999),
+        name: random.string(),
+        group: random.string(),
+        "user-id": random.number(1, 999),
+        ttl: random.number(1, 5000),
+        records: [{
+          name: random.string()
+        }],
+        garbage: random.string()
+      }
+
+      repo.addZone(expectedZone);
+    });
+
+    it('should have added a zone', function () {
+      assert.equal(repo.count(), 1);
+    });
+
+    it('should have set the zone properties', function () {
+      var actualZone = repo.getZone(expectedZone.id);
+
+      assert.equal(actualZone.id, expectedZone.id);
+      assert.equal(actualZone.name, expectedZone.name);
+      assert.equal(actualZone.group, expectedZone.group);
+      assert.equal(actualZone["user-id"], expectedZone["user-id"]);
+      assert.equal(actualZone.ttl, expectedZone.ttl);
+    });
+
+    it('should have not set the non-zone properties', function () {
+      var actualZone = repo.getZone(expectedZone.id);
+
+      assert.equal(actualZone.records.length, 0);
+      assert.equal(actualZone.garbage, undefined);
+    });
+
+  });
+
+  describe('#count with multiple zones', function () {
+
+    var repo;
+    var expectedCount;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      expectedCount = random.number(0, 100);
+
+      for (var i = 0; i < expectedCount; i++) {
+        var zone = {
+          id: i,
+          name: random.string(),
+          group: random.string(),
+          "user-id": random.number(1, 999),
+          ttl: random.number(1, 5000)
+        };
+
+        repo.addZone(zone);
+      };
+    });
+
+    it('should have the expected count of zones', function () {
+      assert.equal(repo.count(), expectedCount);
+    });
+
+  });
+
+  describe('#getZones', function () {
+
+    var repo;
+    var expectedCount;
+    var expectedZones;
+    var actualZones;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      expectedCount = random.number(0, 100);
+      expectedZones = [];
+
+      for (var i = 0; i < expectedCount; i++) {
+        var zone = {
+          id: i,
+          name: random.string(),
+          group: random.string(),
+          "user-id": random.number(1, 999),
+          ttl: random.number(1, 5000),
+          records: []
+        };
+
+        repo.addZone(zone);
+        expectedZones.push(zone);
+      }
+
+      actualZones = repo.getZones();
+    });
+
+    it('should get the expected count of zones', function () {
+      assert.equal(actualZones.length, expectedCount);
+    });
+
+    it('should have the expected zones', function () {
+      assert.deepEqual(actualZones, expectedZones);
+    });
+
+  });
+
+  describe('#reset', function () {
+
+    var repo;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      var createNum = random.number(0, 100);
+
+      for (var i = 0; i < createNum; i++) {
+        var zone = {
+          id: i,
+          name: random.string(),
+          group: random.string(),
+          "user-id": random.number(1, 999),
+          ttl: random.number(1, 5000)
+        };
+
+        repo.addZone(zone);
+      };
+
+      repo.reset();
+    });
+
+    it('should have zero zones', function () {
+      assert.equal(repo.count(), 0);
+    });
+
+  });
+
+  describe('#addZone with same zone id', function () {
+
+    var repo;
+    var zone;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      zone = {
+        id: random.number(1, 9999),
+        name: random.string(),
+        group: random.string(),
+        "user-id": random.number(1, 999),
+        ttl: random.number(1, 5000)
+      };
+
+      repo.addZone(zone);
+    });
+
+    it('should throw an exception when a zone of the same id is added', function () {
+      assert.throws(function () {
+        repo.addZone(zone);
+      }, Error);
+    });
+
+  });
+
+  describe('#addZone with zone and records', function () {
+
+    var repo;
+    var expectedFirstRecord;
+    var expectedSecondRecord;
+    var expectedZone;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      expectedFirstRecord = {
+        id: random.number(1, 999),
+        name: random.string(),
+        data: random.ipAddress(),
+        aux: random.string(),
+        record_type: createRandomRecordType(),
+        redirect_to: random.string(),
+        ttl: random.number(1, 5000),
+        asdasdjkadkj: "this should be ignored"
+      };
+
+      expectedSecondRecord = {
+        id: expectedFirstRecord.id + random.number(1, 999),
+        name: random.string(),
+        data: random.ipAddress(),
+        aux: random.string(),
+        record_type: createRandomRecordType(),
+        redirect_to: random.string(),
+        ttl: random.number(1, 5000),
+        garbage: "this should also be ignored"
+      };
+
+      expectedZone = {
+        id: random.number(1, 9999),
+        name: random.string(),
+        group: random.string(),
+        "user-id": random.number(1, 999),
+        ttl: random.number(1, 5000)
+      }
+
+      repo.addZone(expectedZone, [expectedFirstRecord, expectedSecondRecord]);
+    });
+
+    it('should have added a zone with two records', function () {
+      assert.equal(repo.count(), 1);
+
+      var actualZone = repo.getZone(expectedZone.id);
+      assert.equal(actualZone.records.length, 2);
+    });
+
+    it('should have set the record properties', function () {
+      var actualFirstRecord = repo.getRecord(expectedZone.id, expectedFirstRecord.id);
+
+      assert.equal(actualFirstRecord.id, expectedFirstRecord.id);
+      assert.equal(actualFirstRecord.name, expectedFirstRecord.name);
+      assert.equal(actualFirstRecord.data, expectedFirstRecord.data);
+      assert.equal(actualFirstRecord.aux, expectedFirstRecord.aux);
+      assert.equal(actualFirstRecord.record_type, expectedFirstRecord.record_type);
+      assert.equal(actualFirstRecord.redirect_to, expectedFirstRecord.redirect_to);
+      assert.equal(actualFirstRecord.ttl, expectedFirstRecord.ttl);
+
+      var actualSecondRecord = repo.getRecord(expectedZone.id, expectedSecondRecord.id);
+
+      assert.equal(actualSecondRecord.id, expectedSecondRecord.id);
+      assert.equal(actualSecondRecord.name, expectedSecondRecord.name);
+      assert.equal(actualSecondRecord.data, expectedSecondRecord.data);
+      assert.equal(actualSecondRecord.aux, expectedSecondRecord.aux);
+      assert.equal(actualSecondRecord.record_type, expectedSecondRecord.record_type);
+      assert.equal(actualSecondRecord.redirect_to, expectedSecondRecord.redirect_to);
+      assert.equal(actualSecondRecord.ttl, expectedSecondRecord.ttl);
+    });
+
+    it('should have not set the non-record properties', function () {
+      var actualFirstRecord = repo.getRecord(expectedZone.id, expectedFirstRecord.id);
+      assert.equal(actualFirstRecord.asdasdjkadkj, undefined);
+
+      var actualSecondRecord = repo.getRecord(expectedZone.id, expectedSecondRecord.id);
+      assert.equal(actualSecondRecord.garbage, undefined);
+    });
+
+  });
+
+  describe('#addZone with same record id', function () {
+
+    var repo;
+    var zone;
+    var record;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      zone = {
+        id: random.number(1, 999),
+        name: random.string(),
+        group: random.string(),
+        "user-id": random.number(1, 999),
+        ttl: random.number(1, 5000)
+      };
+
+      record = {
+        id: random.number(1, 999),
+        name: random.string(),
+        data: random.ipAddress(),
+        aux: random.string(),
+        record_type: createRandomRecordType(),
+        redirect_to: random.string(),
+        ttl: random.number(1, 5000)
+      };
+    });
+
+    it('should throw an exception when a record of the same id is added', function () {
+      assert.throws(function () {
+        repo.addZone(zone, [record, record]);
+      }, Error);
+    });
+
+  });
+
+  describe('#updateZone with new zone properties', function () {
+
+    var repo;
+    var zone;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      zone = {
+        id: random.number(1, 9999),
+        name: random.string(),
+        group: random.string(),
+        "user-id": random.number(1, 999),
+        ttl: random.number(1, 5000)
+      };
+
+      repo.addZone(zone);
+
+      zone.name = random.string();
+      zone.group = random.string();
+      zone["user-id"] = random.number(1, 999);
+      zone.ttl = random.number(1, 5000);
+
+      repo.updateZone(zone);
+    });
+
+    it('should have updated the zone', function () {
+      var actualZone = repo.getZone(zone.id);
+
+      assert.equal(actualZone.id, zone.id);
+      assert.equal(actualZone.name, zone.name);
+      assert.equal(actualZone.group, zone.group);
+      assert.equal(actualZone["user-id"], zone["user-id"]);
+      assert.equal(actualZone.ttl, zone.ttl);
+    });
+
+  });
+
+  describe('#updateRecord with new record properties', function () {
+
+    var repo;
+    var zone;
+    var record;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      zone = {
+        id: random.number(1, 9999),
+        name: random.string(),
+        group: random.string(),
+        "user-id": random.number(1, 999),
+        ttl: random.number(1, 5000)
+      };
+
+      record = {
+        id: random.number(1, 999),
+        name: random.string(),
+        data: random.ipAddress(),
+        aux: random.string(),
+        record_type: createRandomRecordType(),
+        redirect_to: random.string(),
+        ttl: random.number(1, 5000)
+      };
+
+      repo.addZone(zone, [record]);
+
+      record.data = random.ipAddress();
+
+      repo.updateRecord(zone.id, record);
+    });
+
+    it('should have updated the record', function () {
+      var actualRecord = repo.getRecord(zone.id, record.id);
+
+      assert.equal(actualRecord.id, record.id);
+      assert.equal(actualRecord.name, record.name);
+      assert.equal(actualRecord.data, record.data);
+      assert.equal(actualRecord.aux, record.aux);
+      assert.equal(actualRecord.record_type, record.record_type);
+      assert.equal(actualRecord.redirect_to, record.redirect_to);
+      assert.equal(actualRecord.ttl, record.ttl);
+    });
+
+  });
+
+  describe('#removeZone', function () {
+
+    var repo;
+    var zone;
+    var record;
+
+    before(function () {
+      repo = new ZoneRepository();
+
+      zone = {
+        id: random.number(1, 999),
+        name: random.string(),
+        group: random.string(),
+        "user-id": random.number(1, 999),
+        ttl: random.number(1, 5000)
+      };
+
+      repo.addZone(zone);
+      repo.removeZone(zone.id);
+    });
+
+    it('should contain no zones', function () {
+      assert.equal(repo.getZones(), 0);
+    });
+
+  });
+
+});
